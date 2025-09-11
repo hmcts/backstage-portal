5c30a3a6ecc73cdfd20f948d44bcd24789b78966
Jack Maloney <jack.maloney1@hmcts.net>
2025-09-11 14:16:28 +0100

    Revert custom sign in

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,15 +7,9 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
-import { createBackendModule } from '@backstage/backend-plugin-api';
-import { microsoftAuthenticator } from '@backstage/plugin-auth-backend-module-microsoft-provider';
-import {
-  authProvidersExtensionPoint,
-  createOAuthProviderFactory,
-} from '@backstage/plugin-auth-node';
-import { stringifyEntityRef, DEFAULT_NAMESPACE } from '@backstage/catalog-model';
 
 const backend = createBackend();
+
 
 backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
@@ -31,6 +25,8 @@ backend.add(
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
+backend.add(import('@backstage/plugin-auth-backend'));
+backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
@@ -70,61 +66,5 @@ backend.add(import('@backstage/plugin-kubernetes-backend'));
 backend.add(import('@backstage/plugin-notifications-backend'));
 backend.add(import('@backstage/plugin-signals-backend'));
 backend.add(import('@backstage/plugin-catalog-backend-module-msgraph'));
-
-
-const customAuth = createBackendModule({
-  // This ID must be exactly "auth" because that's the plugin it targets
-  pluginId: 'auth',
-  // This ID must be unique, but can be anything
-  moduleId: 'custom-auth-provider',
-  register(reg) {
-    reg.registerInit({
-      deps: { providers: authProvidersExtensionPoint },
-      async init({ providers }) {
-        providers.registerProvider({
-          // This ID must match the actual provider config, e.g. addressing
-          // auth.providers.github means that this must be "github".
-          providerId: 'microsoft',
-          // Use createProxyAuthProviderFactory instead if it's one of the proxy
-          // based providers rather than an OAuth based one
-          factory: createOAuthProviderFactory({
-            authenticator: microsoftAuthenticator,
-           async signInResolver({ profile }, ctx) {
-             if (!profile.email) {
-               throw new Error(
-                 'Login failed, user profile does not contain an email',
-               );
-             }
-             // Split the email into the local part and the domain.
-             const [localPart, domain] = profile.email.split('@');
-
-             // Next we verify the email domain. It is recommended to include this
-            //  kind of check if you don't look up the user in an external service.
-             if (domain !== 'hmcts.net') {
-               throw new Error(
-                 `Login failed, '${profile.email}' does not belong to the expected domain`,
-               );
-             }
-
-             // By using `stringifyEntityRef` we ensure that the reference is formatted correctly
-             const userEntity = stringifyEntityRef({
-               kind: 'User',
-               name: localPart,
-               namespace: DEFAULT_NAMESPACE,
-             });
-             return ctx.issueToken({
-               claims: {
-                 sub: userEntity,
-                 ent: [userEntity],
-               },
-             });
-           }
-          }),
-        });
-      },
-    });
-  },
-});
-backend.add(customAuth);
 
 backend.start();
