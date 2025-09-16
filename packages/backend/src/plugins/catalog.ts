3f125ff99db72ad0fb19f6a14cd62bbe312db176
Jack Maloney <jack.maloney1@hmcts.net>
2025-09-16 09:24:33 +0100

    Implement catalog refresh

---
+++ packages/backend/src/plugins/catalog.ts
@@ -0,0 +1,26 @@
+import { ApiCatalogProvider } from './apiCatalogProvider';
+
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha';
+
+const catalogModuleCustomExtensions = createBackendModule({
+  pluginId: 'catalog', // name of the plugin that the module is targeting
+  moduleId: 'custom-extensions',
+  register(env) {
+    env.registerInit({
+      deps: {
+        catalog: catalogProcessingExtensionPoint,
+      },
+      async init({ catalog }) {
+        catalog.addEntityProvider(
+          new ApiCatalogProvider(
+            null as unknown as any,
+            null as unknown as any,
+          ),
+        );
+      },
+    });
+  },
+});
+
+export default catalogModuleCustomExtensions;
