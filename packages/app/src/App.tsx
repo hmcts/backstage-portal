5131842cc086d6bac7a11716c6063c53adbce081
Jack Maloney <jack.maloney1@hmcts.net>
2025-09-17 13:24:38 +0100

    Add homepage with links

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -24,6 +24,8 @@ import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
+import { HomepageCompositionRoot } from '@backstage/plugin-home';
+import { HomePage } from './components/home/HomePage';
 
 import {
   AlertDisplay,
@@ -84,7 +86,9 @@ const app = createApp({
 
 const routes = (
   <FlatRoutes>
-    <Route path="/" element={<Navigate to="catalog" />} />
+    <Route path="/" element={<HomepageCompositionRoot />}>
+        <HomePage />
+    </Route>
     <Route path="/catalog" element={<CatalogIndexPage />} />
     <Route
       path="/catalog/:namespace/:kind/:name"
